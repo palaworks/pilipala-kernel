e7e591fddf86b2c19be6461a44491f262584dcce
Thaumy <Studio_ai@outlook.com>
2021-12-10 09:20:55 +0800

    ~~

--- pilipala.kernel/src/data/casting.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace pilipala.data
-
-[<AutoOpen>]
-module casting =
-
-    open System
-
-    let inline cast (a: 'a) : 'b =
-        downcast Convert.ChangeType(a, typeof<'b>)
--- pilipala.kernel/src/kernel/container/cache/relationTree.fs
+++
@@ -1,1 +0,0 @@
-module internal RelationTree
